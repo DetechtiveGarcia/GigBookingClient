8e31ab6ef99d2c8925b57e394853bb8e35942c8e
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-04 00:03:01 +0200

    feat: add logo, copyright and links with basic block style

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,22 @@
+import ExternalLink from "../external-link/ExternalLink";
+import Logo from "../logo/Logo";
+import "./footer.css";
+export default function Footer() {
+  return (
+    <footer>
+      <div className="center-element-vertical">
+        <Logo />
+
+        <p className="text-dark">&copy; 2026 - All rights reserved</p>
+      </div>
+      <div>
+        <ul>
+            <li><ExternalLink href="www.youtube.com" label="youtube"/></li>
+            <li><ExternalLink href="www.youtube.com" label="youtube"/></li>
+            <li><ExternalLink href="www.youtube.com" label="youtube"/></li>
+            <li><ExternalLink href="www.youtube.com" label="youtube"/></li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
