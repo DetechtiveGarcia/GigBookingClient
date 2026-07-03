f51384c02cfa230a5a77e5d74525cb86dab3f962
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-03 20:41:47 +0200

    chore: cleaned up the header component for readability

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,13 +1,13 @@
+import AppLink from "../app-link/AppLink";
+import Logo from "../logo/Logo";
+import Navigation from "../navigation/Navigation";
 import "./header.css";
 export default function Header() {
   return (
     <header>
-      
-        <span className="ballet-font white">T</span>
-        <span className="pinyon-script-regular white outline-text">e</span>
-        <span className="pinyon-script-regular white">moll</span>
-        <span className="pinyon-script-regular orange">dur</span>
-      
+      <Logo />
+      <Navigation />
+      <AppLink href="/booking" label="boka" linkStyle="app-link-btn" />
     </header>
   );
 }
