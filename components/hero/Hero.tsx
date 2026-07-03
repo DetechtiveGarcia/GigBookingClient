53f56f9179f728808f32970930e75ab210d8512a
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-04 00:07:40 +0200

    feat: add text with styles of the hero

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,9 +1,19 @@
-import heroImg from '../../public/7a7117ea-ae42-467f-b47e-dc0c75c8ae72.jpeg'
-import './hero.css'
-export default function Hero(){
-    return(
-        <section id='hero'>
-            <img src={heroImg.src} alt="" />
-        </section>
-    );
+import "./hero.css";
+export default function Hero() {
+  return (
+    <section className="hero overlay sides-padding">
+      <span className="text-orange letter-spacing">NY ALBUM UTE NU</span>
+      <h1 className="serif text-white">
+        Ember
+        <br />
+        <span className="italic text-orange">Choir</span>
+      </h1>
+      <div className="album-description">
+        <p className="text-dark">
+          Elva spår med långsamt spelad fingerplockad gitarr, inspelade under en
+          enda helg i ett ökenkapell utanför Marfa.
+        </p>
+      </div>
+    </section>
+  );
 }
