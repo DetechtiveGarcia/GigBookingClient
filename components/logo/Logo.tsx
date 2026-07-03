e2e5750e54eb4c92e7f0449aecbc43f781a81b90
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-03 20:42:45 +0200

    feat: add center classname to the logo div for centering in its place

---
+++ components/logo/Logo.tsx
@@ -0,0 +1,15 @@
+import "./logo.css";
+
+export default function Logo() {
+  return (
+    <>
+      <div className="logo center-element-horizontal">
+        {/* <span className="ballet-font text-white">T</span>
+        <span className="pinyon-script-regular text-white outline-text">e</span>
+        <span className="pinyon-script-regular text-white">moll</span>
+        <span className="pinyon-script-regular text-orange">dur</span> */}
+        <p className="serif text-white">Temoll<span className="italic text-orange">dur</span></p>
+      </div>
+    </>
+  );
+}
