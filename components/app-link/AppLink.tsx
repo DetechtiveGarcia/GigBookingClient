12b633066fee1e6c0e2230472ebb321b91fbf928
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-03 20:40:34 +0200

    feat: add styles and props to the app link component

--- components/app-link/AppLink.tsx
+++ components/app-link/AppLink.tsx
@@ -1,7 +1,9 @@
+import './app-link.css'
 type LinkProps = {
     href: string;
     label: string;
+    linkStyle: 'app-link' | 'app-link-btn' 
 }
-export default function AppLink({ href, label }: LinkProps){
-    return <a href={href}>{label}</a>
+export default function AppLink({ href, label, linkStyle }: LinkProps){
+    return <a href={href} className={`${linkStyle} font-uppercase letter-spacing center-element-horizontal`}>{label}</a>
 }
