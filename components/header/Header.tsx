380d93b645babea81b924e5160538ec911ba5bbc
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-03 18:19:14 +0200

    feat: add prototype logo in the header

---
+++ components/header/Header.tsx
@@ -0,0 +1,13 @@
+import "./header.css";
+export default function Header() {
+  return (
+    <header>
+      
+        <span className="ballet-font white">T</span>
+        <span className="pinyon-script-regular white outline-text">e</span>
+        <span className="pinyon-script-regular white">moll</span>
+        <span className="pinyon-script-regular orange">dur</span>
+      
+    </header>
+  );
+}
