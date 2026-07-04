1e1784f609c2df477ad0b2a8ea7e1f0825e3b8df
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-04 16:04:42 +0200

    feat: import the album images (not yet rendered.)
    - add basic jsx for the section header and section description

--- components/sections/discography/Discography.tsx
+++ components/sections/discography/Discography.tsx
@@ -1,6 +1,32 @@
-import './discography.css'
-export default function Discography(){
-    return(
-        <></>
-    );
+import album1 from "@/public/album-1.jpg";
+import album2 from "@/public/album-2.jpg";
+import album3 from "@/public/album-3.jpg";
+import album4 from "@/public/album-4.jpg";
+import SectionHeader from "@/components/section-header/SectionHeader";
+import "./discography.css";
+
+export default function Discography() {
+  return (
+    <section>
+      <div className="discography-container">
+        <div className="header-layout">
+          <SectionHeader
+            sectionLabel="02 / Discography"
+            sectionHeading={
+              <>A decade of {<span className="italic">six strings</span>}.</>
+            }
+          />
+          <p className="text-white text-dark">
+            Four records. Countless late nights. Every release self-produced in
+            collaboration with a small circle of trusted players.
+          </p>
+        </div>
+        <div className="album-grid">
+            <ul>
+
+            </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
