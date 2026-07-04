52a4730e272ca0c84e058496a38d5207992abccd
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-04 23:16:03 +0200

    feat:
    - add form group components
    - add breaks for empty space between form group components

--- components/sections/booking/BookingForm.tsx
+++ components/sections/booking/BookingForm.tsx
@@ -1,14 +1,26 @@
-import './booking-form.css'
-export default function BookingForm(){
-    return (
-        <div className="booking-form-container">
-            <div className='form-header'>
-                <h6 className="text-white serif">Boka privat spelning</h6>
-                <p className='text-dark'>Festivaler, bröllop, sessioner och privata evenemang. Skicka ett meddelande så svarar teamet inom 48 timmar.</p>
-            </div>
-            <form action="">
-                
-            </form>
-        </div>
-    )
+import FormGroup from "@/components/form/FormGroup";
+import "./booking-form.css";
+export default function BookingForm() {
+  return (
+    <div className="booking-form-container">
+      <div className="form-header">
+        <h6 className="text-white serif">Boka privat spelning/lektion</h6>
+        <p className="text-dark">
+          Festivaler, bröllop, sessioner och privata evenemang. Skicka ett
+          meddelande så svarar teamet inom 48 timmar.
+        </p>
+      </div>
+      <form action="">
+        <FormGroup label="namn" />
+        <br />
+        <FormGroup label="email" />
+        <br />
+        <FormGroup label="event datum" />
+        <br />
+        <FormGroup label="stad/plats" />
+        <br />
+        <button type="submit" className="letter-spacing">Skicka</button>
+      </form>
+    </div>
+  );
 }
