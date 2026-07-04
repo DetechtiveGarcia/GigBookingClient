54c4d1adf8ecfc9a096bef4526bd0dfeb61e59cf
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-04 18:02:58 +0200

    feat: add booking component to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Calendar from "@/components/Calendar";
 import Hero from "@/components/sections/hero/Hero";
 import Marquee from "@/components/marquee/Marquee";
 import Listen from "@/components/sections/listen/Listen";
 import Discography from "@/components/sections/discography/Discography";
 import Artist from "@/components/sections/artist/Artist";
+import Booking from "@/components/sections/book-private-show/Booking";
 // import "@fullcalendar/common/main.css";
 // import "@fullcalendar/daygrid/main.css";
 
@@ -15,12 +15,12 @@ export default function Home() {
 
   return (
     <>
-      {/* <Calendar /> */}
       <Hero/>
       <Marquee />
       <Listen/>
       <Discography/>
       <Artist/>
+      <Booking/>
     </>
   );
 }
