cde62ce26fe8a7b593fb59124fef7e0ce0a7d17c
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-04 18:05:01 +0200

    feat: add booking section componetn with its booking-form and section components

---
+++ components/sections/book-private-show/Booking.tsx
@@ -0,0 +1,20 @@
+import SectionHeader from "@/components/section-header/SectionHeader";
+import BookingForm from "./BookingForm";
+import "./booking.css";
+export default function Booking() {
+  return (
+    <section>
+      <div className="booking-container">
+        <SectionHeader
+          sectionLabel="04 / boka"
+          sectionHeading={
+            <>
+              Spelningar <span className="italic">2026</span>
+            </>
+          }
+        />
+        <BookingForm/>
+      </div>
+    </section>
+  );
+}
