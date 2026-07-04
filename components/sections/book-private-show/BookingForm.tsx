13985c6cc1e513a9d5f389d83d292498d0d63abd
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-04 18:05:26 +0200

    feat: add booking form component. Only header as of now, no form yet.

---
+++ components/sections/book-private-show/BookingForm.tsx
@@ -0,0 +1,14 @@
+import './booking-form.css'
+export default function BookingForm(){
+    return (
+        <div className="booking-form-container">
+            <div className='form-header'>
+                <h6 className="text-white serif">Boka privat spelning</h6>
+                <p className='text-dark'>Festivaler, bröllop, sessioner och privata evenemang. Skicka ett meddelande så svarar teamet inom 48 timmar.</p>
+            </div>
+            <form action="">
+                
+            </form>
+        </div>
+    )
+}
