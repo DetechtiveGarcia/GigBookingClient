e9cba9f054afb638c1a7ab75bc6c967d755e6354
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-24 11:59:26 +0200

    feat: add responsive css styles for the booking section. Add Gigs component (with its Gig component) to booking section

--- components/sections/booking/Booking.tsx
+++ components/sections/booking/Booking.tsx
@@ -2,22 +2,27 @@ import SectionHeader from "@/components/section-header/SectionHeader";
 import BookingForm from "./BookingForm";
 import Calendar from "@/components/calendar/Calendar";
 import "./booking.css";
+import Gigs from "@/components/gigs/Gigs";
+
 export default function Booking() {
   return (
     <section className="wrapper" id="booking">
       <div className="booking-container">
-        <SectionHeader
-          sectionLabel="04 / boka"
-          sectionHeading={
-            <>
-              Spelningar <span className="italic">2026</span>
-            </>
-          }
-        />
+        <div>
+          <SectionHeader
+            sectionLabel="04 / boka"
+            sectionHeading={
+              <>
+                Spelningar <span className="italic">2026</span>
+              </>
+            }
+          />
+          <Gigs />
+        </div>
         <BookingForm />
 
-          {/* <Calendar />
-  */}
+        {/* <Calendar />
+         */}
       </div>
     </section>
   );
