20dd4e658b67380763b335f285a5d30ff671171f
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-24 20:15:55 +0200

    refactor: add type DateValue and update then the GigBooking type

--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,15 @@
 export type GigBooking = {
-  id: string;
-  startDate: string;
-  endDate: string;
-  street: string;
-  streetNumber: string;
-  zipCode: string;
-  city: string;
-  clientName: string;
-  clientEmail: string;
-  clientPhone: string;
-  venue: string;
+  _id: string;
+  StartDate: DateValue;
+  EndDate: DateValue;
+  Street: string;
+  StreetNumber: string;
+  ZipCode: string;
+  City: string;
+  ClientName: string;
+  ClientEmail: string;
+  ClientPhone: string;
+  Venue: string;
 };
 
 export type GigBookingRequest = {
@@ -29,3 +29,8 @@ export type LocalStorageSavedGigBookings = {
   gigBookings: GigBooking[];
 }
 
+type DateValue = {
+  DateTime: string;
+  Ticks: number;
+  Offset: number;
+};
