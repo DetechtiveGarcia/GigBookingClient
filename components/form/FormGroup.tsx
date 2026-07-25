ad739f4ba5dce3d80a819c0ceb52af6194ba6bad
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-07-25 10:10:28 +0200

    feat: add ternary operator for text area. Enable dynamic props and add forwardRef for validation from parent component

--- components/form/FormGroup.tsx
+++ components/form/FormGroup.tsx
@@ -1,18 +1,39 @@
-import './form-group.css'
+import React, { forwardRef } from "react";
+import "./form-group.css";
+
 type FormGroupProps = {
   label: string;
   isMessage?: boolean;
-};
-export default function FormGroup({
-  label,
-  isMessage = false,
-}: FormGroupProps) {
-  return (
-    <>
-      <label htmlFor="" className="text-dark uppercase letter-spacing">
-        {label}
-      </label>
-      <input type="text" className='text-white' />
-    </>
-  );
-}
+} & React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement>;
+
+const FormGroup = forwardRef<HTMLInputElement & HTMLTextAreaElement, FormGroupProps>(
+  ({ label, isMessage = false, ...props }, ref) => {
+    return (
+      <div className="form-group-wrapper">
+        <label htmlFor={props.name} className="text-dark uppercase letter-spacing block">
+          {label}
+        </label>
+        {isMessage ? (
+          <textarea
+            id={props.name}
+            ref={ref}
+            className="text-white w-full bg-transparent border-b border-white"
+            {...props}
+          />
+        ) : (
+          <input
+            id={props.name}
+            ref={ref}
+            type="text"
+            className="text-white w-full bg-transparent border-b border-white"
+            {...props}
+          />
+        )}
+      </div>
+    );
+  }
+);
+
+FormGroup.displayName = "FormGroup";
+
+export default FormGroup;
