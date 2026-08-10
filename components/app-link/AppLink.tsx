7d17fa168151672a7d61946a554b8d3000f7b0f6
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-08-10 23:47:13 +0200

    feat (style):
    - make linkStyle prop optional
    - add new prop: className. For more flexability when styling

--- components/app-link/AppLink.tsx
+++ components/app-link/AppLink.tsx
@@ -3,8 +3,9 @@ import './app-link.css'
 type LinkProps = {
     toSectionId: string;
     label: string;
-    linkStyle: 'app-link' | 'app-link-btn' 
+    linkStyle?: 'app-link' | 'app-link-btn';
+    className?: string;
 }
-export default function AppLink({ toSectionId, label, linkStyle }: LinkProps){
-    return <Link href={`/#${toSectionId}`} className={`${linkStyle} font-uppercase letter-spacing `}>{label}</Link>
+export default function AppLink({ toSectionId, label, linkStyle, className }: LinkProps){
+    return <Link href={`/#${toSectionId}`} className={`${linkStyle} ${className} font-uppercase letter-spacing `}>{label}</Link>
 }
